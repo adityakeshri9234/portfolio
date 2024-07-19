c5c8e5f970af596f8f547a4192ee9e41207b892f
Aditya <adityakeshri9234@gmail.com>
2024-07-19 19:59:13 +0530

    second commit

--- portfolio/src/pages/home/home.jsx
+++ portfolio/src/pages/home/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Profile from "../../assets/home.png"
+import Profile from "../../assets/home.jpeg"
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from 'react-icons/fa'
 import "./home.css"
